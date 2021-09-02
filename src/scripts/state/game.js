116a0bcd642d4a88141985306eccb709c6631e01
Vinaya Kumar H <vinaygotit@gmail.com>
2021-09-03 00:03:22 +0530

    Add files via upload

---
+++ src/scripts/state/game.js
@@ -0,0 +1,88 @@
+/*global define */
+define(['jquery', 'underscore', 'easel', 'state/logic/dying', 'state/logic/playing', 'state/logic/starting'], function ($, _, createjs, DyingLogic, PlayingLogic, StartingLogic) {
+    'use strict';
+    var GameStates,
+        instance;
+
+    GameStates = function (game, render) {
+        this.game = game;
+        this.render = render;
+        this.children = [];
+        this.state = new StartingLogic(this.game, this.render, this);
+    };
+
+    GameStates.prototype.switchState = function (state) {
+        var oldState = this.state;
+        this.clear();
+        if (state === 'dying') {
+            this.state = new DyingLogic(this.game, this.render, this, oldState);
+        } else if (state === 'playing') {
+            this.state = new PlayingLogic(this.game, this.render, this, oldState);
+        }
+        this.init();
+    };
+
+    GameStates.prototype.clearChildren = function () {
+        var that = this;
+        _.each(this.children, function (v) {
+            that.render.getStage().removeChild(v);
+        });
+        this.children = [];
+    };
+
+    GameStates.prototype.addChild = function (child) {
+        var stage = this.render.getStage();
+        this.children.push(child);
+        stage.addChild(child);
+        stage.sortChildren(function (a, b) {
+            var az = a.zindex || 0,
+                bz = b.zindex || 0;
+            return az - bz;
+        });
+    };
+
+    GameStates.prototype.removeChild = function (child) {
+        var index = this.children.indexOf(this.children);
+        delete this.children[index];
+        if (index > -1) {
+            this.children.splice(index, 1);
+        }
+        child.removeAllEventListeners();
+        this.render.getStage().removeChild(child);
+    };
+
+    GameStates.prototype.addChildren = function () {
+        var that = this;
+        _.each(this.children, function (v) {
+            that.render.getStage().addChild(v);
+        });
+    };
+
+    GameStates.prototype.init = function () {
+        this.state.createObjects(this.children);
+        this.addChildren();
+    };
+
+    GameStates.prototype.keyDown = function (e) {
+        instance.state.keyDown(e);
+    };
+
+    GameStates.prototype.mouseDown = function (e) {
+        instance.state.mouseDown(e);
+    };
+
+    GameStates.prototype.clear = function () {
+        this.state.clear();
+        this.clearChildren(this.render);
+        createjs.Ticker.removeAllEventListeners();
+        this.render.attach();
+    };
+
+    return function (game, render) {
+        // singleton
+        return instance || (function () {
+                instance = new GameStates(game, render);
+                return instance;
+            }());
+    };
+});
