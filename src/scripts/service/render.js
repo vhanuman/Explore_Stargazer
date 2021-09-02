0db8624d10f3760f1b688b2b315813852f59094a
Vinaya Kumar H <vinaygotit@gmail.com>
2021-09-03 00:02:25 +0530

    Add files via upload

---
+++ src/scripts/service/render.js
@@ -0,0 +1,56 @@
+/*global define */
+define(['jquery', 'underscore', 'easel'], function ($, _, createjs) {
+    'use strict';
+    var RenderService,
+        instance;
+
+    RenderService = function (canvas, width, height) {
+        this.stage = new createjs.Stage(canvas);
+
+        $(window).bind('resize', this.resize.bind(this));
+
+        this.width = width || 1920;
+        this.height = height || 1080;
+        this.attach();
+    };
+
+    RenderService.prototype.attach = function() {
+        createjs.Ticker.setFPS(60);
+        createjs.Ticker.addEventListener('tick', this.update.bind(this));
+    };
+
+    RenderService.prototype.resize = function () {
+        var w = window.innerWidth,
+            h = window.innerHeight,
+            ow = this.width,
+            oh = this.height,
+            stage = instance.stage,
+            scale = Math.min(w / ow, h / oh);
+
+        stage.scaleX = scale;
+        stage.scaleY = scale;
+
+        // adjust canvas size
+        stage.canvas.width = ow * scale;
+        stage.canvas.height = oh * scale;
+
+        stage.update();
+    };
+
+    RenderService.prototype.update = function (event) {
+        this.stage.update(event);
+    };
+
+    RenderService.prototype.getStage = function () {
+        return instance.stage;
+    };
+
+    return function (options) {
+        // singleton
+        return instance || (function () {
+            instance = new RenderService(options);
+            instance.resize();
+            return instance;
+        }());
+    };
+});
